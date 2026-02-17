3f728fb42b5da944a7b31d6f65df1885ea5b32a3
Kozlov <ikozlov@eltaltd.ru>
2026-02-17 10:15:15 +0300

    feature/BA-018: Сделал алиасы для всех поддиректорий

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from 'node:path'
 
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development'
@@ -13,8 +9,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     resolve: {
       alias: {
-        "@": resolve(__dirname, "src"),
-        "@components": resolve(__dirname, "src/components"), 
+        '@': path.resolve(__dirname, './src'),
       },
     },
     esbuild: {
